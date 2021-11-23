ec66ca5ab64d729de894fd36caa3a185fcd627ee
Jul Bar <barankovauv@gmail.com>
2021-11-23 10:04:31 +0300

    small menu slide added

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const links = document.querySelectorAll('.block');
 const icons = document.querySelectorAll('.icon');
 const moveCircle = document.querySelector('.circle');
 const circle = 360;
+const samllCirclItems = document.querySelectorAll('.small-circle-item');
+const smallCircleTimer = setInterval(plussSlides, 5000);
+let slideIndex = 1;
 let arr = [];
 
 //кол-во пунктов меню
@@ -32,9 +35,6 @@ for(let i = 1; i <= len; i++) {
     icon.style.transform = `rotate(${-position}deg)`;
 }
 
-let smallCircleTimeOut;
-
-
 
 links.forEach((link, j) => {
     link.addEventListener('click', (e) => {
@@ -48,13 +48,13 @@ links.forEach((link, j) => {
         //определяем позицию (угол) кликнутого элемента
         let position = arr[attr - 1];
         //на сколько нужно прокрутить меню
-        let rot = circle - position ;
+        let rot = circle - position;
 
         // console.log(position);
         // console.log(rot);
 
         //крутим меню на нужный угол
-        if (rot != 360) {
+        if (!link.classList.contains('active')) {
             moveCircle .style.transform = `rotate(${rot}deg)`;
 
             //крутим иконки, чтобы они смотрелись правильно
@@ -64,53 +64,42 @@ links.forEach((link, j) => {
                 let roll = -arr[i] - rot; //-(arr[i] + corner) - rot боковое расположение
                 icons[i].style.transform = `rotate(${roll}deg)`;
             }
-        }     
+        }   
         
         links.forEach(link=> { // убираем подсветку активного выбора меню
             link.classList.remove('active');
         });
         link.classList.add('active');
 
-        hideImage();
-        showImage(j);
+        hideContent(images);
+        showContent(images, j);
+
+        hideContent(samllCirclItems);
+        showContent(samllCirclItems, j);
     });
 });
 
 
+hideContent(images);
+showContent(images);
 
-hideImage();
-showImage();
 
-function hideImage() { //скрывает вкладку
-    images.forEach(item => { // скрываем отображение вкладки
+function hideContent(array) { //скрывает вкладку
+    array.forEach(item => { // скрываем отображение вкладки
         item.classList.add('hide');
         item.classList.remove('show', 'fadeIn');
-    });
-
-    
+    });  
 }
 
 
-function showImage(j = 0) {
-    images[j].classList.add('show', 'fadeIn'); // показывает определенную вкладку и анимацию
-    images[j].classList.remove('hide');
-    links[j].classList.add('tabheader__item_active'); // делаем айтем менюшки активным(жирным)
+function showContent(array, j = 0) {
+    array[j].classList.add('show', 'fadeIn'); // показывает определенную вкладку и анимацию
+    array[j].classList.remove('hide');
 }
 
-
-
-
-
-
-
-
-
-
-const samllCirclItems = document.querySelectorAll('.small-circle-item');
-
-let slideIndex = 1;
 
 showSlides(slideIndex, samllCirclItems);
+
 
 function showSlides(n, slides) {
     if (n > slides.length) { // если упираемся в правую границу
@@ -120,25 +109,18 @@ function showSlides(n, slides) {
     if(n < 1) {
         slideIndex = slides.length; // если упираемся в левую границу
     }
-    // slides.forEach(item => item.style.opacity = '0'); 
-    // slides.forEach(item => item.style.visibility = 'hidden');
+
     slides.forEach(item => item.classList.remove('fadeIn'));
-    // slides[slideIndex - 1].style.opacity  = '1';
-    // slides[slideIndex - 1].style.visibility = 'visible';
     slides[slideIndex - 1].classList.add('fadeIn');
 }
+
 
 function plussSlides() { //будет переключать слайды при клике
     showSlides(slideIndex += 1, samllCirclItems);
 }
 
-let test = document.querySelector('.test');
 
-test.addEventListener('click', () => {
-    plussSlides();
-});
 
-const smallCircleTimer = setInterval(plussSlides, 5000);
 
 
 
